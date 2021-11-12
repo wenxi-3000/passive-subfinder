6229e7f5ad1d32eb16fb7db724621c8b751087e5
ShadowFlow <shadowflow@MacBook-Pro.lan>
2021-11-12 16:51:05 +0800

    20211112

--- test/testPassive/testPassive.go
+++ test/testPassive/testPassive.go
@@ -3,6 +3,6 @@ package main
 import "github.com/passive-subfinder/passive"
 
 func main() {
-	passive.GetPassiveResult("teambition.com", "/Users/shadowflow/go/pkg/mod/github.com/passive-subfinder/results/teambition.com/tmp")
+	passive.GetPassiveResult("", "")
 
 }
